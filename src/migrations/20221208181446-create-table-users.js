a3c75b6fb33253b5a52a1c9e8d3a3ecb2d22011c
Guidzs <guidz.2004@gmail.com>
2022-12-08 16:25:20 -0300

    requisito 1

--- src/migrations/20221208181446-create-table-users.js
+++ src/migrations/20221208181446-create-table-users.js
@@ -1,8 +1,27 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {await queryInterface.createTable('users', {
-    id: Sequelize.INTEGER
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+      type: Sequelize.INTEGER,
+    },
+    display_name: {
+      type: Sequelize.STRING,
+    },
+    email: {
+      primaryKey: true,
+      type: Sequelize.STRING,
+    },
+    password: {
+      type: Sequelize.STRING
+    },
+    image: {
+      type: Sequelize.STRING,
+    },
   });
   },
 
---
+++ src/migrations/20221208184453-create-table-blog-posts.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blog_posts', {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      content: {
+        type: Sequelize.STRING,
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true,
+      },
+      published: {
+        type: Sequelize.DATE,
+      },
+      updated: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('blog_posts');
+  },
+};
