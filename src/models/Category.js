032a3144fe668d176355ebc3c96594f39330ee7f
Guidzs <guidz.2004@gmail.com>
2022-12-12 23:43:09 -0300

    requisito 7 ok

---
+++ src/models/Category.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  const category = sequelize.define('Category', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: DataTypes.STRING,
+  }, {
+    sequelize,
+  });
+  
+  return category;
+};
